395deb5aeef2e90cf7d13daae6d444dcb95e9902
Richie Schuurman <richie.schuurman@hotmail.com>
2022-03-17 17:00:41 +0100

    added shuffle for answers array

--- src/components/Questions/Question.vue
+++ src/components/Questions/Question.vue
@@ -1,32 +1,72 @@
 <template>
-<h4>Question header</h4>
-<div class="container">
-<div class="show-questions">
-</div>
-<button type="button" class="btn btn-primary"  @click="someFunction">next question</button>
-</div>
+  <h4>Question header</h4>
+  <div class="container">
+    <div id="question" class="show-questions">
+      {{ questionString.value }}
+
+      <button
+        type="button"
+        class="btn btn-primary"
+        @click="loopTroughQuestions"
+      >
+        Next question
+      </button>
+      {{ firstAnwserString.value }}
+      {{ secondAnwserString.value }}
+      {{ thirdAnwserString.value }}
+      {{ fourthAnwserString.value }}
+    </div>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { reactive } from "vue";
+import { shuffle } from "../../utils/shuffle.js";
+import {useRouter} from 'vue-router'
 
-import { reactive } from 'vue';
-import { getUrl } from "../../utils/triviaApi.js";
-import { onBeforeMount } from 'vue';
-import Question from './Question.vue';   
+let questionString = reactive({});
+let firstAnwserString = reactive({});
+let secondAnwserString = reactive({});
+let thirdAnwserString = reactive({});
+let fourthAnwserString = reactive({});
 
-const storedquestions = JSON.parse(localStorage.getItem("questions"));
+let question = JSON.parse(localStorage.getItem("questions"));
+let correctAnswers = JSON.parse(localStorage.getItem("correctAnswer"));
+let incorrectAnswers = JSON.parse(localStorage.getItem("incorrectAnswer"));
+
+const router = useRouter()
 
 loopTroughQuestions();
 
-function someFunction() {
-    console.log("show some text");
-}
+function loopTroughQuestions() {
+  if (correctAnswers != undefined) {
+    let currentQuestionNumber = parseInt(
+      localStorage.getItem("questionNumber")
+    );
+    console.log(question[currentQuestionNumber]);
+    questionString.value = String(question[currentQuestionNumber]);
 
-function loopTroughQuestions(){
-    let currentQuestionNumber = parseInt(localStorage.getItem("questionNumber"));
-    console.log(storedquestions[currentQuestionNumber]);
+    let allAnswers = [];
+
+    let currentCorrectAnswer = correctAnswers[currentQuestionNumber];
+    let currentIncorrectAnswers = incorrectAnswers[currentQuestionNumber];
+
+    allAnswers.push(currentCorrectAnswer);
+
+    currentIncorrectAnswers.forEach((currentIncorrectAnswer) =>
+      allAnswers.push(currentIncorrectAnswer)
+    );
+    allAnswers = shuffle(allAnswers);
+
+    firstAnwserString.value = allAnswers[0];
+    secondAnwserString.value = allAnswers[1];
+    thirdAnwserString.value = allAnswers[2];
+    fourthAnwserString.value = allAnswers[3];
+
     currentQuestionNumber++;
     localStorage.setItem("questionNumber", String(currentQuestionNumber));
-    console.log("something");
+  } else {
+      router.push('/results')
+  }
 }
 </script>
