487741a2e8a9831ee67f0bdf9b12a8833ebc49a9
Richie Schuurman <richie.schuurman@hotmail.com>
2022-03-18 12:07:36 +0100

    added results component

---
+++ src/components/Results/ResultsComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="results-header">
+      <h1>Trivia results</h1>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    @import "../../utils/main.scss";
+</style>
--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,21 +1,12 @@
 <template>
   <section id="results">
-    <div class="container">
-      <h1>Trivia results</h1>
-      <p>
-      Agur everybody monterey swiss taleggio toast comes airedale macaroni
-      paneer toast big triangles. Moved emmental cheesecake boursin queso
-      fontina gouda say. Croque out halloumi triangles fontina when halloumi the
-      on. Chalk cheddar cottage strings cheesecake queso swiss bergkase slices
-      salut. Everyone castello pepper feet caerphilly stinking normandie
-      lancashire.
-    </p>
-    </div>
+      <ResultsComponent/>
       
   </section>
 </template>
 
-<script>
+<script setup>
+import ResultsComponent from "../components/Results/ResultsComponent.vue";
 </script>
 
 <style lang="scss" scoped>
